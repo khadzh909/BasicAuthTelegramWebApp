1a0f47a6ab71b1e70a03e28bd089550f97a90c73
khadzhiev909 <khadzhiev909@gmail.com>
2025-05-26 22:08:15 +0300

    second commit 1) добавление БД 2) создание дто 3) создание пользователя

---
+++ src/main/java/com/example/telegramauth/DTO/UserDTO.java
@@ -0,0 +1,99 @@
+package com.example.telegramauth.DTO;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserDTO {
+    private Long id;
+    private String first_name;
+    private String last_name;
+    private String username;
+    private String language_code;
+    private boolean allows_write_to_pm;
+    private String photo_url;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(Long id, String first_name, String last_name, String username, String language_code, boolean allows_write_to_pm, String photo_url) {
+
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.username = username;
+        this.language_code = language_code;
+        this.allows_write_to_pm = allows_write_to_pm;
+        this.photo_url = photo_url;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getLanguage_code() {
+        return language_code;
+    }
+
+    public void setLanguage_code(String language_code) {
+        this.language_code = language_code;
+    }
+
+    public boolean isAllows_write_to_pm() {
+        return allows_write_to_pm;
+    }
+
+    public void setAllows_write_to_pm(boolean allows_write_to_pm) {
+        this.allows_write_to_pm = allows_write_to_pm;
+    }
+
+    public String getPhoto_url() {
+        return photo_url;
+    }
+
+    public void setPhoto_url(String photo_url) {
+        this.photo_url = photo_url;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "id=" + id +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", username='" + username + '\'' +
+                ", language_code='" + language_code + '\'' +
+                ", allows_write_to_pm=" + allows_write_to_pm +
+                ", photo_url='" + photo_url + '\'' +
+                '}';
+    }
+}
+
+
---
+++ src/main/java/com/example/telegramauth/model/User.java
@@ -0,0 +1,84 @@
+package com.example.telegramauth.model;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String first_name;
+    private String last_name;
+    private String username;
+    private String language_code;
+    private boolean allows_write_to_pm;
+
+    public User() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return allows_write_to_pm == user.allows_write_to_pm && Objects.equals(id, user.id) && Objects.equals(first_name, user.first_name) && Objects.equals(last_name, user.last_name) && Objects.equals(username, user.username) && Objects.equals(language_code, user.language_code);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, first_name, last_name, username, language_code, allows_write_to_pm);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getLanguage_code() {
+        return language_code;
+    }
+
+    public void setLanguage_code(String language_code) {
+        this.language_code = language_code;
+    }
+
+    public boolean isAllows_write_to_pm() {
+        return allows_write_to_pm;
+    }
+
+    public void setAllows_write_to_pm(boolean allows_write_to_pm) {
+        this.allows_write_to_pm = allows_write_to_pm;
+    }
+}
